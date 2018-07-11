6068a7d2b0a83cced30ba948bf985c15a76dc2c4
David Wyrobnik <david@davidwy.com>
2018-07-10 23:50:45 -0400

    ctr mode, week2 assignment complete

--- week2.py
+++ week2.py
@@ -6,7 +6,7 @@ AES_BLOCK_SIZE = 16
 
 def encrypt_cbc_aes(key, pt):
     cipher = AES.new(key, AES.MODE_ECB)
-    iv = Random.get_random_bytes(AES_BLOCK_SIZE)
+    iv = make_iv()
 
     # pad the pt
     padding = AES_BLOCK_SIZE - (len(pt) % AES_BLOCK_SIZE)
@@ -42,13 +42,37 @@ def decrypt_cbc_aes(key, ct):
     return pt
 
 def encrypt_ctr_aes(key, pt):
-    pass
+    iv = make_iv()
+    return iv + ctr_aes(key, iv, pt)
 
 def decrypt_ctr_aes(key, ct):
-    pass
+    return ctr_aes(key, ct[:AES_BLOCK_SIZE], ct[AES_BLOCK_SIZE:])
+
+def ctr_aes(key, iv, text):
+    cipher = AES.new(key, AES.MODE_ECB)
+    output = []
+    for i in range(0, len(text), AES_BLOCK_SIZE):
+        block = text[i:i+AES_BLOCK_SIZE]
+        prf_result = cipher.encrypt(iv)
+        output.append(xor(prf_result, block))
+        iv = increment_string(iv)
+    return ''.join(output)
+
+def make_iv():
+    return Random.get_random_bytes(AES_BLOCK_SIZE)
 
 def xor(block1, block2):
     return ''.join([chr(ord(x) ^ ord(y)) for x, y in zip(block1, block2)])
+
+def increment_string(input):
+    bytelist = [ord(c) for c in input]
+    carry = 1
+    for i in range(len(bytelist) - 1, -1, -1):
+        new_byte = bytelist[i] + carry
+        carry = new_byte >= 256
+        bytelist[i] = new_byte % 256
+    return ''.join([chr(b) for b in bytelist])
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process arguments.')
